a2b31b44b09fa02889288d5a5dc31b5b159f5551
jibrilhaleemat <jibrilhaleemat31@gmail.com>
2026-02-26 11:17:03 +0100

    Added SQL dump file

---
+++ Dump20260226.sql
@@ -0,0 +1,112 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: happy_store
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `id` int NOT NULL,
+  `name` tinytext NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'Bisi Akinola','bisi@gmail.com','2026-02-05 12:30:05','2026-02-05 12:30:05'),(2,'Bello Muhammad','bello@gmail.com','2026-02-05 12:30:05','2026-02-05 12:30:05'),(3,'Sarah Banjo','sarah@gmail.com','2026-02-05 12:30:05','2026-02-05 12:30:05'),(4,'Maryam Abdullahi','maryam@gmail.com','2026-02-05 12:30:05','2026-02-05 12:30:05'),(5,'Ayomide Sanni','ayomide@gmail.com','2026-02-05 12:30:05','2026-02-05 12:30:05');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  `total_amount` float NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `product_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_order` (`product_id`),
+  CONSTRAINT `fk_product_order` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'2',15000,'approved','2026-02-05 12:30:05','2026-02-05 12:30:05',NULL),(2,'1',18000,'pending','2026-02-05 12:30:05','2026-02-05 12:30:05',NULL),(3,'4',24000,'approved','2026-02-05 12:30:05','2026-02-05 12:30:05',NULL),(4,'1',35500,'pending','2026-02-05 12:30:05','2026-02-05 12:30:05',NULL),(5,'3',45000,'approved','2026-02-05 12:30:05','2026-02-05 12:30:05',NULL),(6,'2',15000,'approved','2026-02-25 21:38:41','2026-02-25 21:38:41',1),(7,'4',17000,'approved','2026-02-25 21:38:41','2026-02-25 21:38:41',3),(8,'3',25000,'pending','2026-02-25 21:38:41','2026-02-25 21:38:41',2),(9,'1',35000,'pending','2026-02-25 21:38:41','2026-02-25 21:38:41',4);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `price` float NOT NULL,
+  `created_at` datetime(5) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Bisi Akinola',7000,'2026-02-05 12:30:05.00000','2026-02-05 12:30:05'),(2,'Bello Muhammad',9000,'2026-02-05 12:30:05.00000','2026-02-05 12:30:05'),(3,'Sarah Banjo',12000,'2026-02-05 12:30:05.00000','2026-02-05 12:30:05'),(4,'Maryam Abdullahi',14500,'2026-02-05 12:30:05.00000','2026-02-05 12:30:05'),(5,'Ayomide Sanni',15000,'2026-02-05 12:30:05.00000','2026-02-05 12:30:05');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-26 11:03:23
